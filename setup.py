bff3e59dd35b74bb133edfffb0fd731725875707
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-04-28 13:57:29 +0200

    Add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,27 @@ setup(
     license='MIT',
     author='Joona Lehtomäki',
     author_email='joona.lehtomaki@gmail.com',
-    description='Simple Python CLI tool for creating data packages'
+    description='Simple Python CLI tool for creating data packages',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Data management',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+    ],
+
+
+
 )
