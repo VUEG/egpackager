839620856277c98b391f670d080b3661d86a3fdf
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-04-28 11:39:41 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='egdatapacker',
+    version='0.0.1',
+    packages=['egdatapacker'],
+    url='https://github.com/VUEG/egdatapacker',
+    license='MIT',
+    author='Joona Lehtomäki',
+    author_email='joona.lehtomaki@gmail.com',
+    description='Simple Python CLI tool for creating data packages'
+)
