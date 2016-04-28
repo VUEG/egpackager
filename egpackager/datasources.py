48b481a7fb2cef06da019f99eb11634b17cdb0bc
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-04-28 17:43:37 +0200

    Get values from gspread data source
    Key (dataset name) / value searching implemented.

--- egpackager/datasources.py
+++ egpackager/datasources.py
@@ -12,6 +12,34 @@ class DataSource(object):
         self._data = None
         self.logger = logging.getLogger(__name__)
 
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        assert isinstance(value, DataFrame), "value is not a DataFrame: %r" % value
+        self._data = value
+
+    def get_value(self, key, value):
+        ''' Simple wrapper to fetch a column value based on a key (dataset name).
+
+        The key is hardcoded to "name", so whatever data source is needed it needs to have a column called "name" that
+        uniquely identifies the dataset. If several matches are found, only the first is used.
+
+        :param key: String dataset name.
+        :param value: String column name of which value is returned.
+        :return: String value
+        '''
+        # Check that the key and value are actually found
+        if key not in list(self.data["name"]):
+            self.logger.error("Key '{0}' not found in column 'name'".format(key))
+            return None
+        elif value not in list(self.data.columns):
+            self.logger.error("Value '{0}' not found in column names".format(value))
+            return None
+        else:
+            return self.data.loc[self.data.name == key, value].values[0]
 
 class GspreadDataSource(DataSource):
 
@@ -29,11 +57,12 @@ class GspreadDataSource(DataSource):
         except gspread.exceptions.SpreadsheetNotFound as e:
             raise
 
-    @property
-    def data(self):
-        return self._data
-
     def load_data(self, spreadsheet_name, sheet_no):
+        ''' Load data from a Google Spreadsheet once it has been set up.
+        :param spreadsheet_name: String name of the spreadsheet.
+        :param sheet_no: Index number of the worksheet to be loaded. Named loading not supported yet.
+        :return: Methods is used for it's side effects only, no value is returned.
+        '''
         try:
             # Get the actual data from the worksheet
             dat = self.gc.open(spreadsheet_name).get_worksheet(sheet_no).get_all_values()
