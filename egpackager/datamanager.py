39ea591073339ad86f77c22f7b29436efcf01a0e
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2016-04-29 13:46:49 +0200

    Add metadata and resource distinction in DataManager

--- egpackager/datamanager.py
+++ egpackager/datamanager.py
@@ -25,14 +25,29 @@ class DataManager(object):
             # Remove keyword argument 'type' as it us not needed anymore
             del kwargs['type']
             self.logger.debug('Adding Google Sheets data source')
-            self._data[kwargs['uri']] = GspreadDataSource(*args, **kwargs)
+            self._data['metadata'] = GspreadDataSource(*args, **kwargs)
         elif kwargs['type'] == 'raster':
             del kwargs['type']
             self.logger.debug('Adding raster data source')
-            self._data[kwargs['uri']] = RasterDataSource(*args, **kwargs)
+            self._data['resource'] = RasterDataSource(*args, **kwargs)
         else:
             raise TypeError("Unknown data source type: {0}".format(kwargs['type']))
+        self._data['uri'] = kwargs['uri']
 
     @property
     def data(self):
         return self._data
+
+    def get_metadata_value(self, key, value):
+        return self.data['metadata'].get_value(key, value)
+
+    def get_resource_value(self, key, value):
+        return self.data['resource'].get_value(key, value)
+
+    @property
+    def metadata(self):
+        return self.data['metadata'].data
+
+    @property
+    def resource_metadata(self):
+        return self.data['resource'].data
